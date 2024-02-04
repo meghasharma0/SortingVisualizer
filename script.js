26b68b6691bb3771ea1ce5e6568d9ee8f60fb92e
Megha Sharma <meghaalg7@gmail.com>
2024-02-04 15:32:07 +0530

    bubble sort implementation

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ for (let i = 0; i < n; i++){
     arr[i] = Math.random();
 }
 
+// Bubble sort implementation
+// let swapped = false;
+// for (let i = 0; i < n; i++){
+//     for (let j = 0; j < n-i-1; j++){
+//         if (arr[j] > arr[j+1]){
+//             let temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp;
+//         }
+//     }
+// }
+do{
+    var swapped = false;
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i-1] > arr[i]){
+            swapped = true;
+            [arr[i-1], arr[i]] = [arr[i], arr[i-1]];
+        }
+    }
+}while (swapped);
+
 const container = document.getElementById("container");
 // Creating individual bars with the array
 for (let i = 0; i < arr.length; i++){
