5d2f377223eaa979ce79a15d8fd332e01be37512
Megha Sharma <meghaalg7@gmail.com>
2024-02-04 15:19:51 +0530

    create bars

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const arr = [];
 
 // Setting random numbers between 1 to 10 in array
 for (let i = 0; i < n; i++){
-    arr[i] = Math.floor(Math.random() * 10) + 1;
+    arr[i] = Math.random();
 }
 
+const container = document.getElementById("container");
+// Creating individual bars with the array
+for (let i = 0; i < arr.length; i++){
+    const bar = document.createElement("div");
+    bar.style.height = arr[i]*100 + "%";
+    bar.style.width = "10px";
+    bar.style.background = "black";
+    container.appendChild(bar);
+}
