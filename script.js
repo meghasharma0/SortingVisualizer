8944d4a53d0f3d08fe0b7de46de9159f916e4340
Megha Sharma <meghaalg7@gmail.com>
2024-02-04 15:49:09 +0530

    added animation

--- script.js
+++ script.js
@@ -23,29 +23,51 @@ function init(){
 // }
 
 function play(){
-    bubbleSort(arr);
-    showBars();
+    const copy = [...arr];
+    const swaps = bubbleSort(copy);
+    animate(swaps);
 }
+
+function animate(swaps){
+    if (swaps.length == 0){
+        showBars();
+        return;
+    }
+    const [i, j] = swaps.shift();
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    showBars([i, j]);
+    setTimeout(function(){
+        animate(swaps);
+    }, 500)
+}
+
 function bubbleSort(arr){
+    const swaps = [];
     do{
         var swapped = false;
         for (let i = 1; i < arr.length; i++){
             if (arr[i-1] > arr[i]){
                 swapped = true;
+                swaps.push([i-1, i]);
                 [arr[i-1], arr[i]] = [arr[i], arr[i-1]];
             }
         }
     }while (swapped);
+    return swaps;
 }
 
 // Creating individual bars with the array
-function showBars(){
+function showBars(indices){
     const container = document.getElementById("container");
     container.innerHTML = "";
     for (let i = 0; i < arr.length; i++){
         const bar = document.createElement("div");
         bar.style.height = arr[i]*100 + "%";
         bar.classList.add("bar");
+
+        if (indices && indices.includes(i)){
+            bar.style.background = "red";
+        }
         container.appendChild(bar);
     }
 }
