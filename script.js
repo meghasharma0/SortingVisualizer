f57b09d6c2ab6f2a9d0956fdd3e2a41c6d94e75a
Megha Sharma <meghaalg7@gmail.com>
2024-02-04 15:38:46 +0530

    init and play buttons working

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const n = 10;
 const arr = [];
 
+init();
 // Setting random numbers between 1 to 10 in array
-for (let i = 0; i < n; i++){
-    arr[i] = Math.random();
+function init(){
+    for (let i = 0; i < n; i++){
+        arr[i] = Math.random();
+    }
+    showBars();
 }
 
 // Bubble sort implementation
@@ -17,21 +21,31 @@ for (let i = 0; i < n; i++){
 //         }
 //     }
 // }
-do{
-    var swapped = false;
-    for (let i = 1; i < arr.length; i++){
-        if (arr[i-1] > arr[i]){
-            swapped = true;
-            [arr[i-1], arr[i]] = [arr[i], arr[i-1]];
-        }
-    }
-}while (swapped);
 
-const container = document.getElementById("container");
+function play(){
+    bubbleSort(arr);
+    showBars();
+}
+function bubbleSort(arr){
+    do{
+        var swapped = false;
+        for (let i = 1; i < arr.length; i++){
+            if (arr[i-1] > arr[i]){
+                swapped = true;
+                [arr[i-1], arr[i]] = [arr[i], arr[i-1]];
+            }
+        }
+    }while (swapped);
+}
+
 // Creating individual bars with the array
-for (let i = 0; i < arr.length; i++){
-    const bar = document.createElement("div");
-    bar.style.height = arr[i]*100 + "%";
-    bar.classList.add("bar");
-    container.appendChild(bar);
+function showBars(){
+    const container = document.getElementById("container");
+    container.innerHTML = "";
+    for (let i = 0; i < arr.length; i++){
+        const bar = document.createElement("div");
+        bar.style.height = arr[i]*100 + "%";
+        bar.classList.add("bar");
+        container.appendChild(bar);
+    }
 }
